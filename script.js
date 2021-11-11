1f0f3f1ba980388eec57d074aad77d20f07d7c73
DAVID CHRISTIAN MACHADO <david.m.cristian@gmail.com>
2021-11-11 20:16:37 -0300

    Alteração Página login

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var app = angular.module('mainModule', []);
 
+
 app.controller('mainController', function($scope, $http){ //o scope liga o js e o template
   $scope.nome = 'Valor Inicial';
   //$http.get().success();
